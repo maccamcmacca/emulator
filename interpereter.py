8e5ce318120e3466480582219dbcd22672e71c35
RyanMcaleaney <ryanmcaleaney@linux033.computing.dundee.ac.uk>
2017-11-10 11:54:05 +0000

    I know how to input/output files in python

--- interpereter.py
+++ interpereter.py
@@ -1,3 +1,9 @@
-thefile = open("test.txt", "r")
+#thefile = open("test.txt", "r")
+#for line in thefile:
+#    print line,
+thefile = open("testassem.txt", "w")
+x = raw_input("Enter shit: ")
+thefile.write(x)
+thefile = open("testassem.txt", "r")
 for line in thefile:
     print line,

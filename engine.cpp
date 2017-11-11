01631e9f174020d6b20bf3f4031cd8d9fd471a27
maccamcmacca <ry1nmca@gmail.com>
2017-11-11 02:53:08 +0000

    bintodec is weird, need to fix. Moved some things temp

--- engine.cpp
+++ engine.cpp
@@ -10,18 +10,8 @@ int bin = 8;
 
 int main(){
 
-    for(int i = 0; i<8; i++){
-         for(int j = 0; j<8; j++){
-            if(reg[i][1] == '0' && reg[i][2] == '0' && reg[i][3] == '0'){
-                counter = bin;    
-            }
-
-            if(reg[i][1] == '1' && reg[i][2] == '0' && reg[i][3] == '0'){
-                counter = bin;    
-            }
-        }
-    }
-   load(reg);
-   printMem(reg);
+    cout << bin << endl;
+    bin = bintoDec(bin);
+    cout << endl << bin << endl;
 
 }
--- engine.hpp
+++ engine.hpp
@@ -81,5 +81,24 @@ int bintoDec(int num){
     }
     rem = num % 2;
     bintoDec(num / 2);
+    //num = num;
     cout << rem;
 }
+/*THIS IS A TEMPORARY PLACEHOLDER FOR THIS WHILE I PISS ABOUT WITH THE MAIN FUNCTION
+//THIS ONLY EXISTS TO NOT CLUTTER UP MAIN ATM
+void run(){
+
+for(int i = 0; i<8; i++){
+         for(int j = 0; j<8; j++){
+            if(reg[i][1] == '0' && reg[i][2] == '0' && reg[i][3] == '0'){
+                counter = bin;    
+            }
+
+            if(reg[i][1] == '1' && reg[i][2] == '0' && reg[i][3] == '0'){
+                counter = bin;    
+            }
+        }
+    }
+
+}
+*/ 

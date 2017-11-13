d72e9b631772f8e2c49c5966f80834ad0841e15e
maccamcmacca <ry1nmca@gmail.com>
2017-11-13 17:51:41 +0000

    Useless shit

--- interpereter.py
+++ interpereter.py
@@ -1,6 +1,6 @@
-#thefile = open("test.txt", "r")
-#for line in thefile:
-#    print line,
+thefile = open("test.txt", "r")
+for line in thefile:
+    print line,
 thefile = open("testassem.txt", "w")
 x = raw_input("Enter shit: ")
 thefile.write(x)

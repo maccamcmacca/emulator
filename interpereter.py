4273b5a2504e7d3416e8504a56801c7345ee279f
maccamcmacca <ry1nmca@gmail.com>
2017-11-14 02:23:08 +0000

    Got some manupulation goin here

--- interpereter.py
+++ interpereter.py
@@ -1,3 +1,5 @@
+from bitstring import BitArray 
+
 with open("test.txt") as f:
     content = f.readlines()
 # you may also want to remove whitespace characters like `\n` at the end of each line
@@ -5,13 +7,21 @@ content = [x.strip() for x in content]
 
 
 s = content[0]
-print s[:3]
-
-writeit = open("testassem.txt", "w")
-if s[:3] == "011":
-    s = "JMP"
-writeit.write(s)
+#print s[:3]
 
 readit = open("testassem.txt","r")
 for line in readit:
-    print line,
+    print line
+    r = s[3:]
+    print r
+    b = BitArray(bin=r)
+    print b.uint
+    e = b.uint
+
+writeit = open("testassem.txt", "w")
+if s[:3] == "011":
+    s = "JMP "
+    l = "\n"
+    writeit.writelines(s)
+    writeit.writelines(str(b.uint))
+    writeit.writelines(l)

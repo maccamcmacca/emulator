4969a47efc51153459fe3ff56a787b389298ec2b
maccamcmacca <ry1nmca@gmail.com>
2017-11-14 01:56:59 +0000

    Interpereter.py has functionality

--- interpereter.py
+++ interpereter.py
@@ -1,9 +1,17 @@
-thefile = open("test.txt", "r")
-for line in thefile:
-    print line,
-thefile = open("testassem.txt", "w")
-x = raw_input("Enter shit: ")
-thefile.write(x)
-thefile = open("testassem.txt", "r")
-for line in thefile:
+with open("test.txt") as f:
+    content = f.readlines()
+# you may also want to remove whitespace characters like `\n` at the end of each line
+content = [x.strip() for x in content] 
+
+
+s = content[0]
+print s[:3]
+
+writeit = open("testassem.txt", "w")
+if s[:3] == "011":
+    s = "JMP"
+writeit.write(s)
+
+readit = open("testassem.txt","r")
+for line in readit:
     print line,

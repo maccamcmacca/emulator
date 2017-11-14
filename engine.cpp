46a1b4ea0dd7a988c88176b47cfe952ee0529a17
maccamcmacca <ry1nmca@gmail.com>
2017-11-14 01:26:54 +0000

    Moved framework to engine.hpp

--- engine.cpp
+++ engine.cpp
@@ -10,42 +10,7 @@ int bin = 8;
 
 int main(){
 
-    for(int i = 0; i<8; i++){
-         for(int j = 0; j<8; j++){
-            if(reg[i][1] == '0' && reg[i][2] == '0' && reg[i][3] == '0'){
-                counter = bin;    
-            }
-
-            if(reg[i][1] == '1' && reg[i][2] == '0' && reg[i][3] == '0'){
-                counter = bin;    
-            }
-
-            if(reg[i][1] == '1' && reg[i][2] == '1' && reg[i][3] == '0'){
-                counter = bin;    
-            }
-
-            if(reg[i][1] == '0' && reg[i][2] == '0' && reg[i][3] == '1'){
-                counter = bin;    
-            }
-
-            if(reg[i][1] == '0' && reg[i][2] == '1' && reg[i][3] == '1'){
-                counter = bin;    
-            }
-
-            if(reg[i][1] == '1' && reg[i][2] == '1' && reg[i][3] == '1'){
-                counter = bin;    
-            }
-
-            if(reg[i][1] == '1' && reg[i][2] == '0' && reg[i][3] == '1'){
-                counter = bin;    
-            }
-
-            if(reg[i][1] == '0' && reg[i][2] == '1' && reg[i][3] == '0'){
-                counter = bin;    
-            }
-
-        }
-    }
+   
 
 }
 
--- engine.hpp
+++ engine.hpp
@@ -88,5 +88,41 @@ int bintoDec(int num){
 //THIS ONLY EXISTS TO NOT CLUTTER UP MAIN ATM
 void run(){
 
+ for(int i = 0; i<8; i++){
+         for(int j = 0; j<8; j++){
+            if(reg[i][1] == '0' && reg[i][2] == '0' && reg[i][3] == '0'){
+                counter = bin;    
+            }
 
-*/ 
+            if(reg[i][1] == '1' && reg[i][2] == '0' && reg[i][3] == '0'){
+                counter = bin;    
+            }
+
+            if(reg[i][1] == '1' && reg[i][2] == '1' && reg[i][3] == '0'){
+                counter = bin;    
+            }
+
+            if(reg[i][1] == '0' && reg[i][2] == '0' && reg[i][3] == '1'){
+                counter = bin;    
+            }
+
+            if(reg[i][1] == '0' && reg[i][2] == '1' && reg[i][3] == '1'){
+                counter = bin;    
+            }
+
+            if(reg[i][1] == '1' && reg[i][2] == '1' && reg[i][3] == '1'){
+                counter = bin;    
+            }
+
+            if(reg[i][1] == '1' && reg[i][2] == '0' && reg[i][3] == '1'){
+                counter = bin;    
+            }
+
+            if(reg[i][1] == '0' && reg[i][2] == '1' && reg[i][3] == '0'){
+                counter = bin;    
+            }
+
+        }
+    }
+}
+*/

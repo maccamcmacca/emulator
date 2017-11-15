10073bb3f6c8b1564744328a43c8fa21d5bf8bbe
maccamcmacca <ry1nmca@gmail.com>
2017-11-15 02:27:41 +0000

    Fixed first page input, assembler in alpha 1.0.0

--- interperiter.py
+++ interperiter.py
@@ -1,4 +1,21 @@
 from bitstring import BitArray 
 import interperitercode
 
-#interperiter now works
+x = "error"
+
+asordis = input("1: Assemble\n2: Dissassemble\nPlease enter 1 or 2: ")
+
+if asordis == 1:
+    x = "assemble"
+if asordis == 2:
+    x = "dissassemble"
+
+inputfile = raw_input("Enter the file you would like to " + x + ": ")
+outfile = raw_input("Enter the output file: ")
+
+
+if asordis == 1:
+    interperitercode.assemble(inputfile, outfile)
+if asordis == 2:
+    interperitercode.disassemble(inputfile, outfile)
+print"---------------------------------\n\tFunction Complete\n---------------------------------"
--- interperitercode.py
+++ interperitercode.py
@@ -6,16 +6,13 @@ def disassemble(inputfile, outputfile):
     # you may also want to remove whitespace characters like `\n` at the end of each line
     content = [x.strip() for x in content]
 
-    print content
 
     for w in range(len(content)):
         
         s = content[w]
-        #print s[:3]
-
+        
         readit = open(inputfile, "r")
         for line in readit:
-            print line
             r = s[3:]
             b = BitArray(bin=r)
 
@@ -82,79 +79,47 @@ def assemble(inputfile, outputfile):
     # you may also want to remove whitespace characters like `\n` at the end of each line
     content = [x.strip(" ") for x in content]
 
-    print content
-
     for w in range(len(content)):
         
         s = content[w]
-        print s[3:]
         r = int(s[3:])
         b = "{0:b}".format(r)
-        
-        """readit = open(inputfile, "r")
-        for line in readit:
-            print line,
-        
-            print str(b)"""
         
         writeit = open(outputfile, "a")
         if s[:3] == "JMP":
             s = "000"
-            #l = "\n"
             writeit.write(s)
-            #writeit.writelines(str(b.uint))
-            #writeit.writelines(l)
         
         if s[:3] == "LDN":
             s = "100"
-            #l = "\n"
             writeit.write(s)
-            #writeit.writelines(str(b.uint))
-            #writeit.writelines(l)
         
         if s[:3] == "STO":
             s = "110"
-            #l = "\n"
             writeit.write(s)
-            #writeit.writelines(str(b.uint))
-            #writeit.writelines(l)
+            
 
         if s[:3] == "SUB":
             s = "001"
-            #l = "\n"
             writeit.write(s)
-            #writeit.writelines(b)
-            #writeit.writelines(l)
+            
 
         if s[:3] == "STP":
             s = "011"
-            #l = "\n"
             writeit.write(s)
-            #writeit.writelines(str(b.uint))
-            #writeit.writelines(l)
         
         if s[:3] == "MUL":
             s = "111"
-            #l = "\n"
             writeit.write(s)
-            #writeit.writelines(str(b.uint))
-            #writeit.writelines(l)
 
         if s[:3] == "DIV":
             s = "101"
-            #l = "\n"
             writeit.write(s)
-            #writeit.writelines(str(b.uint))
-            #writeit.writelines(l)
 
         if s[:3] == "ADD":
             s = "010"
-            #l = "\n"
             writeit.write(s)
-            #writeit.writelines(str(b.uint))
-            #writeit.writelines(l)
 
-        #writei = open(outputfile, "a")
         writeit.write(str(b).zfill(5))
         writeit.write("\n")
 

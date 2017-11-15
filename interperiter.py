2a1262cdb0dc7ca8f87da4b8dc4fa8b63d30e7c4
maccamcmacca <ry1nmca@gmail.com>
2017-11-15 03:35:15 +0000

    Spelling fixed and comments added

--- interperiter.py
+++ interperiter.py
@@ -4,21 +4,23 @@ import interperitercode
 x = "error"
 
 while x == "error":
-    asordis = input("1: Assemble\n2: Dissassemble\nPlease enter 1 or 2: ")
+    #asordis means assembler or disassembler
+    asordis = input("1: Assemble\n2: Disassemble\nPlease enter 1 or 2: ")
 
     if asordis == 1:
         x = "assemble"
     if asordis == 2:
-        x = "dissassemble"
+        x = "disassemble"
     else:
         print "Put in 1 or 2 you goddamn commie"
-
+#this means quite literally what it is
 inputfile = raw_input("Enter the file you would like to " + x + ": ")
 outfile = raw_input("Enter the output file: ")
 
-
+#basically does the function that the user selects
 if asordis == 1:
     interperitercode.assemble(inputfile, outfile)
 if asordis == 2:
     interperitercode.disassemble(inputfile, outfile)
+#looks fancy
 print"---------------------------------\n\tFunction Complete\n---------------------------------"

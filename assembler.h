837cbce1788e7f312aa164adccdd09f40a439656
maccamcmacca <ry1nmca@gmail.com>
2018-04-01 02:11:17 +0100

    I'm onto something here

--- assembler.h
+++ assembler.h
@@ -10,14 +10,16 @@ typedef struct memory{
 
 }memory;
 
+
+//prints the memory
 void debug(memory* mem){
 
-    for(int i = 0; i < 32; i++){
+    /* for(int i = 0; i < 32; i++){
         printf("%d", mem->current[i]);
-    }
+    } */
     printf("\n\n");
-    for(int j = 0; j<32; j++){
-        for(int i = 0; i<32; i++){
+    for(int j = 0; j<=31; j++){
+        for(int i = 0; i<=31; i++){
             printf("%d", mem->memory[j][i]);
         }
         printf("\n");
@@ -35,26 +37,29 @@ char* loadToArray(char *fileName, memory* mem){
     if (file == NULL)
         return NULL; //could not open file
 
-    code = malloc(1000);
+    code = malloc(10000);
 
     while ((c = fgetc(file)) != EOF)
     {
         code[n++] = (char) c;
     }
+    
     int d = 0;
-    for(int j = 0; j<32; j++){
-        for(int i = 0; i<32; i++){
+
+    for(int j = 0; j<=32; j++){
+        for(int i = 0; i<=32; i++){
+            
             if(code[d] == '1'){
-                mem->current[d] = 1;
+                mem->memory[j][i] = 1;
             }
             else if(code[d] == '0'){
-                mem->current[d] = 0;
+                mem->memory[j][i] = 0;
             }
             d++;
         }
-        for(int n = 0; n<32; n++){
-        mem->memory[j][n] = mem->current[n];
-        }
+         for(int n = 0; n<32; n++){
+        mem->current[n] = mem->memory[j][n]  ; 
+        } 
     }
 
     for(int i = 0; i<1000; i++){

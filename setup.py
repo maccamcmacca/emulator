d6801c8fe940a80e78a4bdd5060b579017d76c55
maccamcmacca <ry1nmca@gmail.com>
2018-04-20 16:08:18 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+    name='Small Instrution Set Emulator',  
+    version='1.0 Release',
+    packages=['Tkinter',],
+    license='GPL 3.0',
+    author='Ryan McAleaney',
+    requires='Tkinter'
+
+)
